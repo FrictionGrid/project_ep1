050e8f39f48b07a66db75385e79a22c1a10b0331
Frictiongrid <phumiphat.s2310@gmail.com>
2026-09-16 13:56:12 +0700

    level1:env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+  ],
 })
 export class AppModule {}
