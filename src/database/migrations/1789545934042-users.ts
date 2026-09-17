3bea1be65bd16daf0f64acddf8e9058a1e596d09
Frictiongrid <phumiphat.s2310@gmail.com>
2026-09-17 11:08:45 +0700

    level4:run migrate

--- src/database/migrations/1789545934042-users.ts
+++ src/database/migrations/1789545934042-users.ts
@@ -3,9 +3,21 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class Users1789545934042 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE users(
+                id  SERIAL PRIMARY KEY,
+                username VARCHAR(100) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                display_name VARCHAR(100) UNIQUE NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE users`);
     }
 
 }
